d642b87b6d393c3f4bd725122b3d91d0a853cad6
Manuel Velez Martinez <manuelvelez@gmail.com>
2016-02-10 13:37:27 +0100

    chanes of time.sleep for explicit wait from webdriver

--- features/steps.py
+++ features/steps.py
@@ -3,16 +3,23 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 @step(u'Given ikabo is opened')
 def given_ikabo_is_opened(step):
-	time.sleep(2)
+	time.sleep(1)
 
 @step(u'When I configure valo url')
 def when_i_configure_valo_url(step):
-	valoUrl = world.driver.find_element_by_css_selector('[data-type=valoLocation]')
-	valoUrl.clear()
-	valoUrl.send_keys(world.valo_url)
+
+    valo_url = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-type=valoLocation]') )
+    )
+    valo_url.clear()
+    valo_url.send_keys(world.valo_url)
 
 @step(u'And I Configure the tenant string')
 def and_i_configure_the_tenant_string(step):
@@ -36,19 +43,25 @@ def then_i_can_see_the_main_screen_with(step, url, tenant):
 
 
 @step(u'And create a domain in "([^"]*)" called "([^"]*)"')
-def and_create_a_domain_in_group1_called_group2(step, group1, group2):
-    time.sleep(2)
-    world.driver.find_element_by_css_selector('[data-type=new-tab]').click()
-    time.sleep(2)
-    world.driver.find_element_by_xpath('//*[@id="mod-tabs-1"]/div/div/div[1]').click()
+def and_create_a_domain_in_group1_called_group2(step, domain_group, domain_name):
 
-    collectionBox = world.driver.find_element_by_css_selector('[data-type=set-domain-collection]')
-    collectionBox.clear()
-    collectionBox.send_keys(group1)
+    new_tab_element = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-type=new-tab]'))
+    )
+    new_tab_element.click()
 
-    nameBox = world.driver.find_element_by_css_selector('[data-type=set-domain-name]')
-    nameBox.clear()
-    nameBox.send_keys(group2+str(world.scenario_time))
+    new_domain_element = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '//*[@id="mod-tabs-1"]/div/div/div[1]'))
+    )
+    new_domain_element.click()
+
+    collection_box = world.driver.find_element_by_css_selector('[data-type=set-domain-collection]')
+    collection_box.clear()
+    collection_box.send_keys(domain_group)
+
+    name_box = world.driver.find_element_by_css_selector('[data-type=set-domain-name]')
+    name_box.clear()
+    name_box.send_keys(domain_name+str(world.scenario_time))
 
     world.driver.find_element_by_css_selector('[data-type=save-domain]').click()
 
@@ -58,29 +71,41 @@ def then_the_domain_group1_is_saved_in_group2(step, group1, group2):
     domain_name_test = group1+str(world.scenario_time)
     world.driver.find_element_by_css_selector('[data-id=domains]').click()
     time.sleep(1)
-    domain_link = world.driver.find_element_by_css_selector('[data-name='+group1+str(world.scenario_time)+']')
+    domain_link = world.driver.find_element_by_css_selector('[data-name=' + domain_name_test + ']')
+
     text = " "+group2
-    domain_in_collection=world.driver.find_elements_by_xpath('//ul[@class="ui-level2_open"]/li[text()="'+text+'"]/../ul/li[@data-name="'+group1+str(world.scenario_time)+'"]')
-    assert len(domain_in_collection) == 1
-    domain_in_collection[0].click()
+    domain_in_collection_element = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//ul[@class="ui-level2_open"]/li[text()="'+text+'"]/../ul/li[@data-name="' + domain_name_test + '"]'))
+    )
+    assert True, domain_in_collection_element
+
+    domain_in_collection_element.click()
     domain_name = world.driver.find_element_by_xpath('//h1[@data-type="set-page-name" and not(ancestor::div[contains(@style,"display: none")])]').text
     assert domain_name == group1+str(world.scenario_time)
 
 
 @step(u'And create an uncontextualized notebook called "([^"]*)" in "([^"]*)"')
 def and_create_an_uncontextualized_notebook_called_group1_in_group2(step, group1, group2):
-    time.sleep(2)
-    world.driver.find_element_by_css_selector('[data-type=new-tab]').click()
-    time.sleep(2)
-    world.driver.find_element_by_xpath('//*[@id="mod-tabs-1"]/div/div/div[2]').click()
 
-    time.sleep(2)
-    collectionBox = world.driver.find_element_by_xpath('//input[@data-type="set-notebook-collection" and not(ancestor::div[contains(@style,"display: none")])]')
-    collectionBox.clear()
-    collectionBox.send_keys(group2)
-    nameBox = world.driver.find_element_by_xpath('//input[@data-type="set-notebook-name" and not(ancestor::div[contains(@style,"display: none")])]')
-    nameBox.clear()
-    nameBox.send_keys(group1+str(world.scenario_time))
+    new_tab_element = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-type=new-tab]'))
+    )
+    new_tab_element.click()
+
+    new_notebook_element = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '//*[@id="mod-tabs-1"]/div/div/div[2]'))
+    )
+    new_notebook_element.click()
+
+    collection_box_element = WebDriverWait(world.driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@data-type="set-notebook-collection" and not(ancestor::div[contains(@style,"display: none")])]'))
+    )
+    collection_box_element.clear()
+    collection_box_element.send_keys(group2)
+
+    name_box = world.driver.find_element_by_xpath('//input[@data-type="set-notebook-name" and not(ancestor::div[contains(@style,"display: none")])]')
+    name_box.clear()
+    name_box.send_keys(group1+str(world.scenario_time))
     world.driver.find_element_by_xpath('//button[@data-type="save-notebook" and not(ancestor::div[contains(@style,"display: none")])]').click()
 
 @step(u'Then the notebook "([^"]*)" is saved in "([^"]*)" and contextualized to "([^"]*)"')
